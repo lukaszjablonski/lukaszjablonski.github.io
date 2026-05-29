b611a4d5ab5f7659150a3eb7706b8e311b7666c1
Lukasz Jablonski <lukasz.jablonski@wp.eu>
2026-05-29 10:57:55 +0200

    Updated github.js

--- script/github.js
+++ script/github.js
@@ -2,6 +2,11 @@ jQuery.githubUser = function(username, callback) {
   //jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
   jQuery.getJSON("https://api.github.com/users/" + username + "/repos", callback);
 }
+
+jQuery.githubUserGists = function(username, callback) {
+  //jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
+  jQuery.getJSON("https://api.github.com/users/" + username + "/gists", callback);
+}
  
 jQuery.fn.loadRepositories = function(username) {
   this.html("<span>Querying GitHub for repositories...</span>");
@@ -14,6 +19,22 @@ jQuery.fn.loadRepositories = function(username) {
     var list = $('<dl/>');
     target.empty().append(list);
     $(repos).each(function() {
+      list.append('<dt><a href="'+ this.svn_url +'">' + this.name + '</a></dt>');
+      list.append('<dd>' + this.description + '</dd>');
+    });
+  });
+
+  jQuery.fn.loadGists = function(username) {
+  this.html("<span>Querying GitHub for gists...</span>");
+ 
+  var target = this; 
+  $.githubUser(username, function(data) {
+    var gists = data;
+    sortByNumberOfWatchers(gists);
+ 
+    var list = $('<dl/>');
+    target.empty().append(list);
+    $(gists).each(function() {
       list.append('<dt><a href="'+ this.svn_url +'">' + this.name + '</a></dt>');
       list.append('<dd>' + this.description + '</dd>');
     });
