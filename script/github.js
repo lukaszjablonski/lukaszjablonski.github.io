e28351bf5dc83c3ef4be114b5d9748cb983b65df
Lukasz Jablonski <lukasz.jablonski@wp.eu>
2026-05-29 12:33:37 +0200

    Updated github.js

--- script/github.js
+++ script/github.js
@@ -33,7 +33,7 @@ jQuery.fn.loadRepositoriesGists = function(username, r, g) {
     var list = $('<dl/>');
     target.append(list);
     $(gists).each(function() {
-      list.append('<dt><a href="'+ this.html_url +'">' + this.filename + '</a></dt>');
+      list.append('<dt><a href="'+ this.html_url +'">' + this.files[0].filename + '</a></dt>');
       list.append('<dd>' + this.description + '</dd>');
     });
   });
